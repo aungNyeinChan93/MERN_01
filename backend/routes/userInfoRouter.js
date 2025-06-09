16a8c84366109f5ea5eed5af48cb5b9ec425e551
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-06-09 07:54:35 +0630

    update userInfoRouter

--- backend/routes/userInfoRouter.js
+++ backend/routes/userInfoRouter.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { JWT } from "../utils/helper.js";
-
+import User from '../models/User.js'
 const usreInfoRouter = Router();
 
-usreInfoRouter.get('/', (req, res, next) => {
+usreInfoRouter.get('/', async (req, res, next) => {
     const { token } = req.cookies;
     if (!token) {
         return res.json({});
     }
     const userInfo = JWT.getToken(token, process.env.SECRECT_KEY);
+    const auth = await User.findById(userInfo.id, { password: false, __v: false }).lean();
     return res.json({
         mess: 'success',
-        result: userInfo
+        result: auth
     });
 })
 
